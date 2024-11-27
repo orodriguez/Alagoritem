fb7122570c9dfd8dceaf32d29de064769b247d81
Crypt1cMS <dylan.minaya12@gmail.com>
2024-11-27 17:07:23 -0400

    Homework Completed - Dylan Minaya

--- DataStructures/LnkList.cs
+++ DataStructures/LnkList.cs
@@ -46,7 +46,39 @@ public class LnkList
 
     public void Insert(int index, int value)
     {
-        throw new NotImplementedException();
+        if (index == 0)
+        {
+            Prepend(value);
+            return;
+        }
+        
+        var current = _head;
+        int currentIndex = 0;
+        
+        while (current != null && currentIndex < index)
+        {
+            if (currentIndex == index - 1)
+            {
+                var newNode = new LnkListNode(value);
+
+                newNode.Next = current.Next;
+                if (current.Next != null)
+                    current.Next.Previous = newNode;
+
+                newNode.Previous = current;
+                current.Next = newNode;
+
+                if (newNode.Next == null)
+                    _last = newNode;
+
+                return;
+            }
+
+            current = current.Next;
+            currentIndex++;
+        }
+        
+        throw new ArgumentOutOfRangeException(nameof(index));
     }
 
     public IEnumerable<int> ToArray() => 
@@ -54,6 +86,20 @@ public class LnkList
 
     public int[] ToReversedArray()
     {
-        throw new NotImplementedException();
+        if (_head == null)
+        {
+            return Array.Empty<int>();
+        }
+        
+        var node = new List<int>();
+        var current = _last;
+
+        while (current != null)
+        {
+            node.Add(current.Value);
+            current = current.Previous;
+        }
+        
+        return node.ToArray();
     }
 }
