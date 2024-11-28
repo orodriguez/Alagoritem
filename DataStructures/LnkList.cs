d6f6a6db3548faee9d07e439c00b9411b7fd3aff
Moises Menendez <manuelmma96@gmail.com>
2024-11-28 15:21:51 -0400

    Fix error related to private property

--- DataStructures/LnkList.cs
+++ DataStructures/LnkList.cs
@@ -13,7 +13,7 @@ public class LnkList
 
     public LnkList(params int[] values)
     {
-        foreach (var value in values) 
+        foreach (var value in values)
             Add(value);
     }
 
@@ -22,7 +22,7 @@ public class LnkList
     {
         if (_head == null)
         {
-            _head = _last =  new LnkListNode(value);
+            _head = _last = new LnkListNode(value);
             return;
         }
 
@@ -34,7 +34,7 @@ public class LnkList
     {
         if (_head == null)
         {
-            _head = _last =  new LnkListNode(value);
+            _head = _last = new LnkListNode(value);
             return;
         }
 
@@ -46,14 +46,54 @@ public class LnkList
 
     public void Insert(int index, int value)
     {
-        throw new NotImplementedException();
+        if (index == 0)
+        {
+            _head = new LnkListNode(value, _head);
+            if (_last == null) _last = _head;
+            return;
+        }
+
+        var current = _head;
+        int currentIndex = 0;
+
+        while (currentIndex < index - 1 && current != null)
+        {
+            current = current.Next;
+            currentIndex++;
+        }
+
+        if (current == null)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        var newNode = new LnkListNode(value, current.Next);
+        current.Next = newNode;
+
+        if (newNode.Next == null)
+        {
+            _last = newNode;
+        }
     }
 
-    public IEnumerable<int> ToArray() => 
+    public IEnumerable<int> ToArray() =>
         _head == null ? Array.Empty<int>() : _head.ToArray();
 
     public int[] ToReversedArray()
     {
-        throw new NotImplementedException();
+        if (_head == null) return Array.Empty<int>();
+
+        var result = new List<int>();
+        var current = _head;
+
+        while (current != null)
+        {
+            result.Insert(0, current.GetValue());  
+            current = current.GetNext();         
+        }
+
+        return result.ToArray();
     }
 }
+
+
--- DataStructures/LnkListNode.cs
+++ DataStructures/LnkListNode.cs
@@ -1,16 +1,22 @@
 namespace DataStructures;
-
-internal class LnkListNode
+public class LnkListNode
 {
-    private int Value { get; }
+    private int Value { get; } 
     public LnkListNode? Next { get; set; }
     public LnkListNode? Previous { get; set; }
-    
+
     public LnkListNode(int value, LnkListNode? next = null, LnkListNode? previous = null)
     {
         Value = value;
         Next = next;
         Previous = previous;
+    }
+    
+    public int GetValue() => Value;
+    public LnkListNode? GetNext() => Next;
+    public void SetNext(LnkListNode? next)
+    {
+        Next = next;
     }
     
     public int[] ToArray()
